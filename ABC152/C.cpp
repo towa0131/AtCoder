c1269d4d2eb28ccbbc000ed0b380c7ee38e935dc
towa0131 <towa.towa0131@gmail.com>
2020-02-13 15:23:20 +0900

    [Add] ABC152を追加

---
+++ ABC152/C.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define MOD 1e9 + 7
+#define PI 3.141592653589793238
+#define pb push_back
+#define ALL(V) (V).begin(), (V).end()
+#define FOR(i, a, b) for(int i = (a); i < (b); ++i)
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long long int lint;
+typedef vector<ll> VL;
+typedef vector<VL> VVL;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+ll gcd(ll a, ll b) { return (a % b == 0 ? b : gcd(b, a % b)); }
+ll lcm(ll a, ll b) { return a * b / gcd(a, b); }
+ll char_to_ll(char c) { return (int)(c - '0'); }
+ll string_to_ll(string s) { return char_to_ll(atoi(s.c_str())); }
+bool is_prime(ll x){ if(x <= 1) return false; for(ll i = 2; sqrt(i) <= x; i++) if(x % i == 0) return false; return true; }
+
+int main(){
+    ll N;
+    cin >> N;
+    VL P(N);
+    FOR(i, 0, N) cin >> P[i];
+
+    ll m = P[0];
+    ll ans = 1;
+    for (int i = 1; i < N; i++){
+        m = min(m, P[i]);
+        if(P[i] <= m) ans++;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
