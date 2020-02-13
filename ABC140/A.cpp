95c08b6d756021efb543eabbb52140890858f21d
towa0131 <towa.towa0131@gmail.com>
2020-02-13 15:21:10 +0900

    [Add] ABC140を追加

---
+++ ABC140/A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define MOD 1e9 + 7
+#define PI 3.141592653589793238
+#define pb push_back
+#define ALL(V) (V).begin(), (V).end()
+#define FOR(i, a, b) for(int i = (a); i < (b); ++i)
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long long int lint;
+typedef vector<ll> VL;
+typedef vector<VL> VVL;
+typedef vector<int> VI;
+ll gcd(ll a, ll b) { return (a % b == 0 ? b : gcd(b, a % b)); }
+ll lcm(ll a, ll b) { return a * b / gcd(a, b); }
+
+int main(){
+    ll A;
+    cin >> A;
+
+    cout << pow(A, 3) << endl;
+
+    return 0;
+}
