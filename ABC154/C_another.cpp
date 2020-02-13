6c8c6fa1b25f432f9cfe351e255f09a094b960a3
towa0131 <towa.towa0131@gmail.com>
2020-02-13 15:24:13 +0900

    [Add] ABC154を追加

---
+++ ABC154/C_another.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define MOD 100000007
+#define PI 3.141592653589793238
+#define pb push_back
+#define ALL(V) (V).begin(), (V).end()
+#define FOR(i, a, b) for(int i = (a); i < (b); ++i)
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long long int lint;
+typedef vector<ll> VL;
+typedef vector<VL> VVL;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+ll gcd(ll a, ll b) { return (a % b == 0 ? b : gcd(b, a % b)); }
+ll lcm(ll a, ll b) { return a * b / gcd(a, b); }
+ll char_to_ll(char c) { return (int)(c - '0'); }
+ll string_to_ll(string s) { return char_to_ll(atoi(s.c_str())); }
+bool is_prime(ll x){ if(x <= 1) return false; for(ll i = 2; sqrt(i) <= x; i++) if(x % i == 0) return false; return true; }
+
+
+int main(){
+    ll N;
+    cin >> N;
+    VL A(N);
+    FOR(i, 0, N) cin >> A[i];
+
+    ll S = A.size();
+    sort(ALL(A));
+    A.erase(unique(ALL(A)), A.end());
+
+    cout << (S == A.size() ? "YES" : "NO") << endl;
+
+    return 0;
+}
